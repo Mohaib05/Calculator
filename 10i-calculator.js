1ca627336b891d39dd03ae3ec4754cb4ecf56af7
Mohaib05 <mohaibali664@gmail.com>
2024-09-02 21:18:07 +0500

    Added original files back

---
+++ 10i-calculator.js
@@ -0,0 +1,17 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+displayCalculation();
+
+function updateCalculation(value) {
+  calculation += value;
+
+  localStorage.setItem('calculation', calculation);
+
+  displayCalculation();
+
+}
+
+
+function displayCalculation(){
+  document.querySelector('.js-calculation').innerHTML = calculation
+}
